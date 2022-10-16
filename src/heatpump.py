23ae375e41d37f2952465f60b10b2e6871e53355
Mark Krautheim <mark.krautheim@gmail.com>
2022-10-16 14:00:54 +0000

    Dev changes

--- src/heatpump.py
+++ src/heatpump.py
@@ -1,15 +1,41 @@
+from datetime import datetime
 import paramiko
 
+class Reading():
+    def __init__(self, record, channel):
+        self.record = record
+        self.channel = channel
+        self.datetimestr = self.record.split()[0]
+        self.datetime = datetime.fromisoformat(self.datetimestr.split('.')[0])
+        self.amps = float(self.record.split()[self.channel])
+
+    @property
+    def timedelta(self):
+        return (datetime.now() - self.datetime).seconds
+
+
 class HeatPump():
-    def __init__(self, ip, user, key):
+    def __init__(self, which, ip, user="pi", key="./id_rsa_rpi", channel=4):
+        """Connect to a Raspberry Pi that is reading
+        heat pump current settings via a XXX HAT to
+        get useful data about the heat pump state.
+        """
         self.client = paramiko.client.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         pkey = paramiko.RSAKey.from_private_key_file("./id_rsa_rpi")
         self.client.connect(ip, username=user, pkey=pkey)
+        self.channel = channel
+        self.which = which
 
     @property
     def is_on(self):
         pass
+
+    def get_last_reading(self):
+        cmd = f"tail -n1 {self.which}_heat_pump_ct_readings.log"
+        stdin, _stdout, _stderr = self.client.exec_command(cmd)
+        reading = Reading(_stdout.read().decode(), self.channel)
+        return reading
 
     @property
     def is_defrosting(self):
@@ -17,11 +43,6 @@ class HeatPump():
         pass
 
 if __name__ == "__main__":
+    which = 'house'
     ip = "192.168.1.9"
-    key = "./id_rsa_rpi"
-    user = "pi"
-    hp = HeatPump(ip, user, key)
-    stdin, _stdout,_stderr = hp.client.exec_command('tail -n1 house_heat_pump_ct_readings.log')
-    # hp.client.exec_command("df")
-    print(_stdout.read().decode())
-    hp.client.close()
+    hp = HeatPump(which, ip)
